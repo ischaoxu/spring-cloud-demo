90a74380a36ad96c811bebf40b84946a73b1b0ce
kutake <1244453574@qq.com>
2023-05-24 20:21:27 +0800

    2023/5/24 20:21 spring-cloud

---
+++ cloudalibaba_provider_payment9001/src/main/java/com/kutake/springcloud/CloudalibabaProviderPayment9001.java
@@ -0,0 +1,11 @@
+package com.kutake.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudalibabaProviderPayment9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudalibabaProviderPayment9001.class, args);
+    }
+}
